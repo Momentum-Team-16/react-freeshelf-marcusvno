58fc542147a0962c49fe5bb13d0e45d379c1b325
Marcus Nogueira <marcusnogueira23@gmail.com>
2023-01-23 13:54:01 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import placeholder from "./no-cover-image.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const fixBrokenImage = (e) => { e.target.src = placeholder; }
+
+
+function App({ bookData }) {
+    return (
+        <div>
+            <h1>Freeshelf</h1>
+            <div className="grid">
+            {bookData.map((data) => (
+                <div className="wrapper">
+                    <div className="imageLoc">
+                        <img src={data.coverImageUrl} alt="cover" onError={fixBrokenImage}/>
+                    </div>
+                    <div className="bookInfo">
+                        <ul>
+                            <li><h3>{data.title}</h3></li>
+                            <li className="author">by {data.author}</li>
+                            <li><p>{data.shortDescription}</p></li>
+                            <li><ExpandedInfo 
+                                url={data.url} 
+                                publisher={data.publisher} 
+                                pubDate={data.publicationDate} 
+                                detailedDesc={data.detailedDescription}/>
+                            </li>
+                        </ul>
+                            
+                    </div>
+                </div>
+                
+            ))}
+            <footer></footer>
+            </div>
+        </div>
+    );
 }
+
+
+function ExpandedInfo({url, publisher, pubDate, detailedDesc}){
+    const [isExpanded, setExpansion] = useState(false);
+    const buttonName = isExpanded ? "Less" : "More";
+
+    return (
+        <div>
+            <button onClick={() => setExpansion(!isExpanded)}><strong>{buttonName} Info</strong></button>
+            {isExpanded && (
+                <div className="expandedBox">
+                    <h4>From the Back Cover:</h4>
+                    <p className="longDesc">{detailedDesc}</p>
+                    {publisher ? <p><strong>Publisher:</strong> {publisher}</p> : null}
+                    {pubDate ? <p><strong>Published:</strong> {pubDate}</p> : null}
+                    {url ? <a href={url} target="_blank" rel="noreferrer"><button><strong>Get Book</strong></button></a> : null}
+
+                </div>
+                
+            )}
+        </div>
+        
+    )
+    
+
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import data from './bookData.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App bookData={data}/>
   </React.StrictMode>
 );
 
